1a64bca9715d7632d52a92f53775133a6a2a6131
maria-canals <mariacanals23@gmail.com>
2021-01-18 14:01:55 +0100

    solved using vue.js

--- js/app.js
+++ js/app.js
@@ -1,33 +1,34 @@
-// Tu código aquí
-const lowerCount = document.querySelector('.prevBtn');
-const addCount = document.querySelector('.nextBtn');
-const textCounter = document.querySelector('#counter');
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      textoActual: "",
+      green: false,
+      red: false,
+    };
+  },
+  methods: {
+    updateCounter() {
+      this.counter++;
+      if (this.counter === 0) {
+        this.green = false;
+        this.red = false;
+      } else if (this.counter > 0) {
+        this.green = true;
+        this.red = false;
+      }
+    },
+    removeCounter() {
+      this.counter--;
+      if (this.counter === 0) {
+        this.green = false;
+        this.red = false;
+      } else if (this.counter < 0) {
+        this.green = false;
+        this.red = true;
+      }
+    },
+  },
+});
 
-let counter = 0;
-
-lowerCount.addEventListener('click', decrement);
-addCount.addEventListener('click', increment);
-
-
-function decrement() {
-    counter--
-    textCounter.textContent = counter;
-    checkNumber()
-}
-
-function increment() {
-    counter++
-    textCounter.textContent = counter;
-    checkNumber()
-}
-
-function checkNumber() {
-    const deleteClassList = textCounter.classList.remove('red','green','black');
-    if(counter < 0) {
-        textCounter.classList.add('red');
-    } else if (counter > 0) {
-        textCounter.classList.add('green');
-    } else {
-        textCounter.classList.add('black');
-    }
-}
+app.mount("#container");
